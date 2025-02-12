1394ae853a713b56c23c0be41026962a8c046d38
karpovz <60101357+karpovz@users.noreply.github.com>
2025-02-12 12:20:53 +0000

    fix head data

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,9 +12,9 @@ export const viewport: Viewport = {
 }
 
 export const metadata: Metadata = {
-  title: 'Nim - Personal website template',
+  title: 'Zahar Karpov',
   description:
-    'Nim is a free and open-source personal website template built with Next.js 15, React 19 and Motion-Primitives.',
+    'Design Engineer',
 }
 
 const geist = Geist({
